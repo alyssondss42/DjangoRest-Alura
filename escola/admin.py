645a1f2dc9ab3e67c65f5b5475082d77fe5d4eb8
alyssondss42 <alyssondssoares@gmail.com>
2023-10-29 16:59:18 -0300

    Add modelo de matricula ao banco

--- escola/admin.py
+++ escola/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 
 # Register your models here.
@@ -9,11 +9,23 @@ class Alunos(admin.ModelAdmin):
     search_fields = ('nome',)
     list_per_page = 20
 
+
 admin.site.register(Aluno, Alunos)
+
 
 class Cursos(admin.ModelAdmin):
     list_display = ('id', 'codigo_curso', 'descricao')
     list_display_links = ('id', 'codigo_curso')
     search_fields = ('codigo_curso', )
 
+
 admin.site.register(Curso, Cursos)
+
+
+class Matriculas(admin.ModelAdmin):
+    list_display = ('id', 'aluno', 'curso', 'periodo')
+    list_display_links = ('id', )
+
+
+admin.site.register(Matricula, Matriculas)
+
