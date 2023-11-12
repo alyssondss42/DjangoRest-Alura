a98d5612bfee1be508598eb67ca82f14b5556ac3
alyssondss42 <alyssondssoares@gmail.com>
2023-11-12 18:33:32 -0300

    Add versionamento da API

--- escola/views.py
+++ escola/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from rest_framework import viewsets, generics
 from escola.models import Aluno, Curso, Matricula
 from .serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, \
-    ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+    ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer, AlunoSerializerV2
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -13,9 +13,14 @@ from rest_framework.permissions import IsAuthenticated
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
     queryset = Aluno.objects.all()
-    serializer_class = AlunoSerializer
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.request.version == 'v2':
+            return AlunoSerializerV2
+        else:
+            return AlunoSerializer
 
 
 class CursosViewSet(viewsets.ModelViewSet):
