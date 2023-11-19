d35b170e887564c822dc8b3c9dab460805df359b
alyssondss42 <alyssondssoares@gmail.com>
2023-11-19 17:40:46 -0300

    Add url nova de admin para seguranca

--- setup/settings.py
+++ setup/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)+vw&9d8*-lhj)f=&frg8&y8ga_9f#m8pl8nv*opnf3h8!b@ab'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'escola',
     'corsheaders',
+    'admin_honeypot',
 ]
 
 MIDDLEWARE = [
